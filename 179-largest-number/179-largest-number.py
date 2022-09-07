2da79ad9cd71a2c5b5795882c0cf693a2801901e
yoonseok ham <54228425+ha-mulan@users.noreply.github.com>
2022-09-07 15:00:32 +0900

    Time: 72 ms (38.43%), Space: 13.9 MB (68.05%) - LeetHub

--- 179-largest-number/179-largest-number.py
+++ 179-largest-number/179-largest-number.py
@@ -1,7 +1,8 @@
 class Solution:
 
     def largestNumber(self, nums: List[int]) -> str:
-        str_nums = list(map(lambda x: (str(x) * 100)[:100], nums))
+        str_nums = list(
+            map(lambda x: (str(x) * (100 // len(str(x)) + 1))[:100], nums))
         answer_list = sorted(list(zip(str_nums, nums)), reverse=True)
 
         return str(int(''.join(str(num) for _, num in answer_list)))
