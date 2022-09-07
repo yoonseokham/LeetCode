3ab4b66757e43aaa816249f0654c601e537da808
yoonseok ham <54228425+ha-mulan@users.noreply.github.com>
2022-09-07 14:28:04 +0900

    Time: 159 ms (36.92%), Space: 16.8 MB (8.69%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,11 +1,14 @@
 import collections
+
+
 class Solution:
+
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         valueIndex = collections.defaultdict(list)
-        
+
         for index, num in enumerate(nums):
             valueIndex[num].append(index)
-        
+
         keySet = set(valueIndex.keys())
         for key in keySet:
             if target - key in keySet:
