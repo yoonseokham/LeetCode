a07e73926776a0d960bf83f4cb45003c07b3a83a
yoonseok ham <54228425+ha-mulan@users.noreply.github.com>
2022-09-11 17:42:25 +0900

    Time: 1238 ms (7.30%), Space: 16.1 MB (91.25%) - LeetHub

--- 1054-distant-barcodes/1054-distant-barcodes.py
+++ 1054-distant-barcodes/1054-distant-barcodes.py
@@ -15,27 +15,28 @@ class Solution:
         even_list = []
         is_odd_turn = True
 
-        def get_append_list(odd_list, even_list):
-            return odd_list if len(odd_list) < len(even_list) else even_list
-
-        def get_overflow_list(odd_list, even_list):
-            return odd_list if len(odd_list) >= len(even_list) else even_list
+        def get_next_list(getSmall=True):
+            returnBigger = lambda a, b: a if len(a) >= len(b) else b
+            returnSmaller = lambda a, b: a if len(a) < len(b) else b
+            if getSmall:
+                return returnSmaller
+            return returnBigger
 
         while data_keys:
-            current_turn = get_append_list(odd_list, even_list)
+            current_turn = get_next_list()(odd_list, even_list)
             current_key = data_keys.popleft()
             for _ in range(data[current_key]):
                 current_turn.append(current_key)
 
-        over_flow_list = get_overflow_list(odd_list, even_list)
-        small_list = get_append_list(odd_list, even_list)
+        big_list = get_next_list(False)(odd_list, even_list)
+        small_list = get_next_list()(odd_list, even_list)
         third_list = []
-        if len(over_flow_list) - len(small_list) >= 2:
-            over_flow_list, third_list = over_flow_list[:len(
-                small_list)], over_flow_list[len(small_list):]
+        if len(big_list) - len(small_list) >= 2:
+            big_list, third_list = big_list[:len(small_list
+                                                )], big_list[len(small_list):]
         answer = []
-        for i in range(len(over_flow_list)):
-            for any_list in (over_flow_list, small_list, third_list):
+        for i in range(len(big_list)):
+            for any_list in (big_list, small_list, third_list):
                 if i < len(any_list):
                     answer.append(any_list[i])
         return answer
