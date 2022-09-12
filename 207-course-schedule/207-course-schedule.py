77f0cf11dbbd10f3055a41bc799f500c32e74e33
yoonseok ham <54228425+ha-mulan@users.noreply.github.com>
2022-09-12 16:23:44 +0900

    Time: 216 ms (18.98%), Space: 17.5 MB (11.30%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.py
@@ -0,0 +1,33 @@
+import collections
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = collections.defaultdict(list)
+        for prerequisite in prerequisites:
+            end, start = prerequisite
+            graph[start].append(end)
+        
+        def isCycle():
+            visited = collections.defaultdict(bool)
+            
+            def dfsSearch(start):
+                isCycle = False 
+                def dfs(current_node):
+                    nonlocal isCycle
+                    visited[current_node] = 'visiting'
+                    for node in graph[current_node]:
+                        if not visited[node]:
+                            dfs(node)
+                        elif visited[node] == 'visiting':
+                            isCycle = True
+                    visited[current_node] = 'finished'
+                dfs(start)
+                return isCycle
+            
+            for i in range(numCourses):
+                if not visited[i]:
+                    result = dfsSearch(i)
+                    if result:
+                        return False
+            return True
+        return isCycle()
