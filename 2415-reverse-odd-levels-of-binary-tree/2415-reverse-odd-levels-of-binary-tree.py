37fe9e3b50791a6e60b3f66441b4c5cb9861cc61
yoonseok ham <54228425+ha-mulan@users.noreply.github.com>
2022-09-25 17:10:54 +0900

    Time: 4384 ms (19.20%), Space: 20.9 MB (13.64%) - LeetHub

---
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.py
@@ -0,0 +1,45 @@
+import collections
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.nodeInfo = collections.namedtuple('nodeInfo', 'node level')
+
+    def get_ordered_list_of_odd_level_node(self, node):
+        level_node = collections.defaultdict(list)
+        q = collections.deque([self.nodeInfo(node, 0)])
+
+        while q:
+            currentNode, currentLevel = q.popleft()
+            if currentLevel % 2:
+                level_node[currentLevel].append(currentNode.val)
+            for child in [currentNode.left, currentNode.right]:
+                if child:
+                    q.append(self.nodeInfo(child, currentLevel + 1))
+        return level_node
+
+    def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        level_node = self.get_ordered_list_of_odd_level_node(root)
+        for key in level_node.keys():
+            level_node[key].reverse()
+
+        q = collections.deque([self.nodeInfo(root, 0)])
+        index = 0
+
+        while q:
+            currentNode, currentLevel = q.popleft()
+            if currentLevel % 2:
+                currentNode.val = level_node[currentLevel][index]
+                index += 1
+            if not currentLevel % 2:
+                index = 0
+            for child in [currentNode.left, currentNode.right]:
+                if child:
+                    q.append(self.nodeInfo(child, currentLevel + 1))
+        return root
