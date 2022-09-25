c6ecd9b89eb184d4d3ff810451fd46fffb872261
yoonseok ham <54228425+ha-mulan@users.noreply.github.com>
2022-09-25 17:38:50 +0900

    Time: 4918 ms (5.02%), Space: 20.7 MB (49.20%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.py
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.py
@@ -10,36 +10,25 @@ import collections
 class Solution:
     def __init__(self):
         self.nodeInfo = collections.namedtuple('nodeInfo', 'node level')
-
-    def get_ordered_list_of_odd_level_node(self, node):
+    
+    def reverseBinaryTreeByBFS(self, node):
         level_node = collections.defaultdict(list)
         q = collections.deque([self.nodeInfo(node, 0)])
-
+        previousLevel = 0
         while q:
+            if not previousLevel%2 and q[0].level%2:
+                start = 0
+                end = len(q) - 1
+                while start < end:
+                    q[start].node.val, q[end].node.val = q[end].node.val, q[start].node.val
+                    start += 1
+                    end -= 1
             currentNode, currentLevel = q.popleft()
-            if currentLevel % 2:
-                level_node[currentLevel].append(currentNode.val)
+            previousLevel = currentLevel
             for child in [currentNode.left, currentNode.right]:
                 if child:
                     q.append(self.nodeInfo(child, currentLevel + 1))
-        return level_node
 
     def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        level_node = self.get_ordered_list_of_odd_level_node(root)
-        for key in level_node.keys():
-            level_node[key].reverse()
-
-        q = collections.deque([self.nodeInfo(root, 0)])
-        index = 0
-
-        while q:
-            currentNode, currentLevel = q.popleft()
-            if currentLevel % 2:
-                currentNode.val = level_node[currentLevel][index]
-                index += 1
-            else:
-                index = 0
-            for child in [currentNode.left, currentNode.right]:
-                if child:
-                    q.append(self.nodeInfo(child, currentLevel + 1))
+        self.reverseBinaryTreeByBFS(root)
         return root
