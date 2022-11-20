b14bf7375a02b7f2f6df58400a3c69e7c1cf281d
yoonseok ham <54228425+ha-mulan@users.noreply.github.com>
2022-11-20 17:35:19 +0900

    Time: 3484 ms (28.57%), Space: 139.3 MB (14.29%) - LeetHub

---
+++ 2476-closest-nodes-queries-in-a-binary-search-tree/2476-closest-nodes-queries-in-a-binary-search-tree.py
@@ -0,0 +1,48 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+import functools
+import bisect
+
+class Solution:
+    def closestNodes(self, root: Optional[TreeNode], queries: List[int]) -> List[List[int]]:
+        datas = []
+        def visit_all_bst(node):
+            if not node:
+                return
+            datas.append(node.val)
+            visit_all_bst(node.left)
+            visit_all_bst(node.right)
+        visit_all_bst(root)
+        datas.sort()
+        print(datas)
+        
+        def lower_bound(target):
+            start = 0
+            end = len(datas) - 1
+            answer  = 10**6 + 1
+            while start <= end:
+                mid = (start + end)//2
+                if target <= datas[mid]:
+                    answer = min(answer,datas[mid])
+                    end = mid -1
+                else:
+                    start = mid + 1
+            return -1 if answer == 10**6 + 1 else answer
+
+        def upper_bound(target):
+            start = 0
+            end = len(datas) - 1
+            answer  = -1
+            while start <= end:
+                mid = (start + end)//2
+                if datas[mid] <= target:
+                    answer = max(answer,datas[mid])
+                    start = mid + 1
+                else:
+                    end = mid - 1
+            return answer
+        return ((upper_bound(query), lower_bound(query)) for query in queries)
