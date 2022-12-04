44ae1ac9327570b3a49b46583482160fe4308dc7
yoonseok ham <54228425+ha-mulan@users.noreply.github.com>
2022-12-04 17:59:25 +0900

    Time: 3176 ms (77.78%), Space: 80.5 MB (11.11%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -1,10 +1,11 @@
 import collections
+
+
 class union_find(object):
-    def __init__(self,n):
-        self.parent = [i for i in range(n+1)]
-    def __call__(self,node1,node2):
-        pass
-    def connectXY(self,x,y):
+    def __init__(self, n):
+        self.parent = [i for i in range(n + 1)]
+
+    def connectXY(self, x, y):
         x = self.parentX(x)
         y = self.parentX(y)
         if x > y:
@@ -12,32 +13,33 @@ class union_find(object):
         if x < y:
             self.parent[y] = x
 
-    def parentX(self,x):
+    def parentX(self, x):
         if x == self.parent[x]:
             return x
         self.parent[x] = self.parentX(self.parent[x])
         return self.parent[x]
+
+
 class Solution:
-            
     def minScore(self, n: int, roads: List[List[int]]) -> int:
-        graph = collections.defaultdict(lambda:collections.defaultdict(bool))
+        graph = collections.defaultdict(lambda: collections.defaultdict(bool))
         root_info = union_find(n)
-        for start,end,cost in roads:
-            root_info.connectXY(start,end)
+        for start, end, cost in roads:
+            root_info.connectXY(start, end)
             if graph[start][end]:
-                graph[start][end] = min(cost,graph[start][end])
-                graph[end][start] = min(cost,graph[end][start])         
+                graph[start][end] = min(cost, graph[start][end])
+                graph[end][start] = min(cost, graph[end][start])
             else:
                 graph[start][end] = cost
                 graph[end][start] = cost
         connected_with_1 = set()
-        answer = 10**4+1
-        for i in range(1,n+1):
+        answer = 10**4 + 1
+        for i in range(1, n + 1):
             if root_info.parentX(i) == 1:
                 connected_with_1.add(i)
-        
-        for start,dicts in graph.items():
-            for end,cost in dicts.items():
+
+        for start, dicts in graph.items():
+            for end, cost in dicts.items():
                 if start in connected_with_1 and end in connected_with_1:
-                    answer = min(answer,cost)
+                    answer = min(answer, cost)
         return answer
