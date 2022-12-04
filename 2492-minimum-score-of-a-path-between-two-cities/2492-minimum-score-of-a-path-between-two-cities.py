92c3c4c38b79d177d14931ed5f8a4696cdda01c7
yoonseok ham <54228425+ha-mulan@users.noreply.github.com>
2022-12-04 17:33:00 +0900

    Time: 6141 ms (11.11%), Space: 79.2 MB (11.11%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -0,0 +1,44 @@
+import collections
+class Solution:
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+        graph = collections.defaultdict(lambda:collections.defaultdict(bool))
+        
+        parent = [i for i in range(n+1)]
+        def parentX(x):
+            if parent[x] == x:
+                return x
+            parent[x] = parentX(parent[x])
+            return parent[x]
+        
+        def connectXY(x,y):
+            x = parentX(x)
+            y = parentX(y)
+            if x == y:
+                return True
+            elif x > y:
+                parent[x] = y
+            else:
+                parent[y] = x
+
+        
+        for start,end,cost in roads:
+            connectXY(start,end)
+            if graph[start][end]:
+                graph[start][end] = min(cost,graph[start][end])
+                graph[end][start] = min(cost,graph[end][start])         
+            else:
+                graph[start][end] = cost
+                graph[end][start] = cost
+        connected_with_1 = set()
+        answer = 10**4+1
+        for i in range(1,n+1):
+            if parentX(i) == 1:
+                connected_with_1.add(i)
+        
+        for start,dicts in graph.items():
+            for end,cost in dicts.items():
+                if cost:
+                    print(start, end , cost)
+                    if start in connected_with_1 and end in connected_with_1:
+                        answer = min(answer,cost)
+        return answer
