5b24df95ee87fce21df86e4e0918d66e6c634603
yoonseok ham <54228425+ha-mulan@users.noreply.github.com>
2023-01-16 10:29:00 +0900

    Time: 2671 ms (88.89%), Space: 36.7 MB (11.11%) - LeetHub

--- 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.py
+++ 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.py
@@ -1,14 +1,6 @@
 import collections
 
 class Solution:
-    Point = collections.namedtuple('Point','i j')
-    def getPoints(self, querie):
-        start_i,start_j,end_i,end_j = querie
-        points = []
-        for i in range(start_i,end_i+1):
-            for j in range(start_j,end_j+1):
-                points.append(self.Point(i,j))
-        return points
             
     def rangeAddQueries(self, n: int, queries: List[List[int]]) -> List[List[int]]:
         box = [[0 for _ in range(n+1)] for _ in range(n)]
