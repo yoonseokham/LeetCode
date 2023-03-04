91190fdb7311f761c0b607710fad5bbc05420003
yoonseok ham <54228425+ha-mulan@users.noreply.github.com>
2023-03-04 15:26:34 +0900

    Time: 6136 ms (6.87%), Space: 15.5 MB (7.83%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,21 +1,18 @@
 import collections 
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        if all(nums):
+        if all(nums) or len(nums)==1:
             return True
-        visit = [False for _ in nums]
         visitable = set([0])
         queue = collections.deque([0])
         while queue:
             index = queue.popleft()
             visitable.discard(index)
-            if index<len(visit):
-                visit[index] = True
-            if visit[-1]:
-                return True
             for i in range(index+1,index+nums[index]+1):
-                if i not in visitable:
+                if i == len(nums)-1:
+                    return True
+                if i not in visitable and index<len(nums):
                     visitable.add(i)
                     queue.append(i)
-        return visit[-1]
+        return False
             
