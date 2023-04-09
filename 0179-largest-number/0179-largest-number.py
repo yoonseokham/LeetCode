01f9103ff2b2815f0fbfdf92d9049c7487ea5f4d
yoonseok ham <54228425+ha-mulan@users.noreply.github.com>
2023-04-09 14:31:41 +0900

    Time: 40 ms (68.72%), Space: 13.8 MB (95.64%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,6 +1,6 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        answer = "".join(i for _,i in sorted(list((i*100,i) for i in map(str, nums)),reverse=True))
+        answer = "".join(i for _,i in sorted(list((i*(100//len(i)),i) for i in map(str, nums)),reverse=True))
         if answer.startswith('0'):
             return '0'
         return answer
