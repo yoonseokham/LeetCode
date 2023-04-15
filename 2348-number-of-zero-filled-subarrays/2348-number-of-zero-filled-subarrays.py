c7539a2f5899f41ccc7223ab36c67336dafe6df2
yoonseok ham <54228425+ha-mulan@users.noreply.github.com>
2023-04-15 14:45:33 +0900

    Time: 1140 ms (20.65%), Space: 24.6 MB (27.98%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.py
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.py
@@ -2,13 +2,16 @@ class Solution:
     def zeroFilledSubarray(self, nums: List[int]) -> int:
         isLastZero = False
         zeroCountList = [0 for i in range(len(nums))]
+        answer = 0
         for index, num in enumerate(nums):
             if not num:
                 if not isLastZero:
                     isLastZero=True
                     zeroCountList[index] = 1
+                    answer += zeroCountList[index]
                 else:
                     zeroCountList[index] = zeroCountList[index-1] + 1
+                    answer += zeroCountList[index]
             else:
                 isLastZero = False
-        return sum(zeroCountList)
+        return answer
